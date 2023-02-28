a97ae93c2c5ff9662e1e91e2e42302756fff397a
rahatsikz <rascal6271@gmail.com>
2023-02-28 20:35:39 +0600

    mongoDB added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
@@ -11,6 +12,31 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Mobile resale server is working fine");
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.htrvoxc.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const productCollection = client
+      .db("secondTimeAround")
+      .collection("products");
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 app.listen(port, () => {
   console.log(`buying second hand phone on port ${port}`);
