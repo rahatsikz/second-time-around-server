4c3c65f8849f81f08cd1e2b7548a2ac5dd058a5f
rahatsikz <rascal6271@gmail.com>
2023-03-02 10:14:02 +0600

    order adding to database api created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
@@ -27,6 +27,7 @@ async function run() {
       .db("secondTimeAround")
       .collection("products");
     const userCollection = client.db("secondTimeAround").collection("users");
+    const orderCollection = client.db("secondTimeAround").collection("orders");
 
     /* categorized product api */
     app.get("/products/:id", async (req, res) => {
@@ -34,15 +35,49 @@ async function run() {
       const query = { categoryID: parseInt(id) };
       // console.log(query);
       const products = await productCollection.find(query).toArray();
-      res.send(products);
+
+      const unpurchased = products.filter(
+        (product) => product.isProductPurchased === false
+      );
+      res.send(unpurchased);
     });
 
     /* add users to database */
     app.post("/users", async (req, res) => {
       const info = req.body;
       console.log(info);
+      const filter = { email: info.email };
+      const existedUser = await userCollection.findOne(filter);
+      if (existedUser) {
+        return res.send({ message: "This user already have been added" });
+      }
       const user = await userCollection.insertOne(info);
       res.send(user);
+    });
+
+    /* add orders to database */
+    app.post("/orders", async (req, res) => {
+      const orderDetail = req.body;
+      const order = await orderCollection.insertOne(orderDetail);
+      res.send(order);
+    });
+
+    app.put("/productstate", async (req, res) => {
+      const id = req.query.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const info = req.body;
+      const updateDoc = {
+        $set: {
+          isProductPurchased: info.purchaseStatus,
+        },
+      };
+      const result = await productCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
     });
   } finally {
   }
