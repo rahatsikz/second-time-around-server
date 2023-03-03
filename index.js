225c30c3979d3996fa907409da908a3e94bde8d9
rahatsikz <rascal6271@gmail.com>
2023-03-03 20:46:33 +0600

    stripe payment added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -28,6 +28,9 @@ async function run() {
       .collection("products");
     const userCollection = client.db("secondTimeAround").collection("users");
     const orderCollection = client.db("secondTimeAround").collection("orders");
+    const paymentCollection = client
+      .db("secondTimeAround")
+      .collection("payments");
 
     /* categorized product api */
     app.get("/products/:id", async (req, res) => {
@@ -40,6 +43,14 @@ async function run() {
         (product) => product.isProductPurchased === false
       );
       res.send(unpurchased);
+    });
+
+    /* filter user by role */
+    app.get("/users", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      res.send(result);
     });
 
     /* add users to database */
@@ -60,6 +71,64 @@ async function run() {
       const orderDetail = req.body;
       const order = await orderCollection.insertOne(orderDetail);
       res.send(order);
+    });
+
+    /* get orders by email */
+    app.get("/orders", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const orders = await orderCollection.find(query).toArray();
+      res.send(orders);
+    });
+
+    /* get orders by id */
+    app.get("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const order = await orderCollection.findOne(query);
+      res.send(order);
+    });
+    /* get price */
+    app.post("/create-payment-intent", async (req, res) => {
+      const price = req.body.price;
+      const amount = price * 100;
+
+      // Create a PaymentIntent with the order amount and currency
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    /* payment post */
+    app.post("/payment", async (req, res) => {
+      const info = req.body;
+      const payment = await paymentCollection.insertOne(info);
+      const id = info.orderID;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          transactionID: info.transaction,
+        },
+      };
+      const updatedOrder = await orderCollection.updateOne(filter, updatedDoc);
+      const query = { name: info.device };
+      const paymentStatus = {
+        $set: {
+          isProductPurchased: true,
+        },
+      };
+      const updatedProduct = await productCollection.updateOne(
+        query,
+        paymentStatus
+      );
+      res.send(payment);
     });
 
     app.put("/productstate", async (req, res) => {
